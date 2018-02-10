cedf05fbd9fc5f6250da16d6a8025d609d363701
marco_saletta <marco.saletta@banzai.it>
2018-02-10 17:42:59 +0100

    Add for setup.py

---
+++ marcohelper/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='marcohelper',
+      version='0.1',
+      description='Test for marcohelper',
+      url='https://github.com/SalMarco/marcohelper',
+      author='Marco Saletta',
+      author_email='marco.saletta@gmail.com',
+      license='MIT',
+      packages=['marcohelper'],
+      zip_safe=False)
